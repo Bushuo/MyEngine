ee438848efb2d5cb8be4b983a60934c889f75716
Paul Buschmann <pbuschmann.mmt-b2013@fh-salzburg.ac.at>
2016-03-21 04:16:21 +0100

    setup glew middleware and made first triangle in OpenGL Window

---
+++ ProjectFiles/SandboxGame/MyGlWindow.h
@@ -0,0 +1,13 @@
+#ifndef SANDBOX_MY_GL_WINDOW_H
+#define SANDBOX_MY_GL_WINDOW_H
+#include <QtOpenGL\qglwidget>
+
+class MyGlWindow : public QGLWidget
+{
+	GLuint vertexBufferID;
+protected:
+	void initializeGL();
+	void paintGL();
+};
+
+#endif
--- ProjectFiles/SandboxGame/SandboxGame.cpp
+++ ProjectFiles/SandboxGame/SandboxGame.cpp
@@ -1,8 +1,14 @@
 #include <Qt\qapplication.h>
+#include <QtGui\qwidget.h>
+#include "MyGlWindow.h"
 
 int main(int argc, char* argv[])
 {
 	QApplication application(argc, argv);
+
+	// get a window
+	MyGlWindow myGlWindow;
+	myGlWindow.show();
 
 	// return error code
 	return application.exec();
